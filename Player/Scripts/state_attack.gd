41c1900fc60f2926ec7719a76b3737a97a5f4d19
eapzzz <125407845+eapzzz@users.noreply.github.com>
2026-05-11 17:13:08 +0200

    E07 - Addressing Issues
    I also added every asset needed in the future into "AARPG Assets" Folder

--- Player/Scripts/state_attack.gd
+++ Player/Scripts/state_attack.gd
@@ -11,7 +11,7 @@ var attacking: bool = false
 
 @onready var walk: State = $"../Walk"
 @onready var idle: State_Idle = $"../Idle"
-@onready var hurt_box: HurtBox = $"../../Interactions/HurtBox"
+@onready var hurt_box: HurtBox = %AttackHurtBox
 
 ## What happens when the player enters this State?
 func Enter() -> void:
