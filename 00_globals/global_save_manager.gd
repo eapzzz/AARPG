e062c4e8d89e3198deef5afa48a1a4a5ab655fc4
eapzzz <125407845+eapzzz@users.noreply.github.com>
2026-05-25 20:06:57 +0200

    E21 - Persistent Data

--- 00_globals/global_save_manager.gd
+++ 00_globals/global_save_manager.gd
@@ -71,3 +71,14 @@ func update_scene_path() -> void:
 func update_item_data() -> void:
 	current_save.items = PlayerManager.INVENTORY_DATA.get_save_data()
 	pass
+
+
+func add_persistent_value(value: String) -> void:
+	if check_persistent_value(value) == false:
+		current_save.persistance.append(value)
+	pass
+
+
+func check_persistent_value(value: String) -> bool:
+	var p = current_save.persistance as Array
+	return p.has(value)
